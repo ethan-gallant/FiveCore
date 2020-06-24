259a249e7dc560dac4da007cf8301c05beb5eed9
Ethan Gallant <ethan@exclnetworks.com>
2020-06-24 01:14:42 -0400

    Push initial files

---
+++ src/main/java/dev/excl/mc/fivecore/FiveCore.java
@@ -0,0 +1,17 @@
+package dev.excl.mc.fivecore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FiveCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/dev/excl/mc/fivecore/database/PunishmentsDatabase.java
@@ -0,0 +1,5 @@
+package dev.excl.mc.fivecore.database;
+
+public class PunishmentsDatabase {
+
+}
