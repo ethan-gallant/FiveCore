318c4405237c8c99f0fcb5137f04435841304c27
Cameron <github@strixdesigns.com>
2020-06-24 21:52:11 -0700

    update dependencies

--- src/main/java/dev/excl/mc/fivecore/FiveCore.java
+++ src/main/java/dev/excl/mc/fivecore/FiveCore.java
@@ -1,17 +1,26 @@
 package dev.excl.mc.fivecore;
+import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FiveCore extends JavaPlugin {
-
+    Logger Logger = Bukkit.getLogger();
+    ConsoleCommandSender clogger = this.getServer().getConsoleSender(); // must type .sendMessage(color + "Message") after it
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        clogger.sendMessage(ChatColor.RED + "---------------------------------------");
+        clogger.sendMessage(ChatColor.GREEN + "FiveCore Has Been Enabled");
+        clogger.sendMessage(ChatColor.RED + "---------------------------------------");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        clogger.sendMessage(ChatColor.GREEN + "---------------------------------------");
+        clogger.sendMessage(ChatColor.RED + "FiveCore Has Been Disabled");
+        clogger.sendMessage(ChatColor.GREEN + "---------------------------------------");
     }
 }
