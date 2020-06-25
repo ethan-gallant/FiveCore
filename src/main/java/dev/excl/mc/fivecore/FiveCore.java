5c2abfa30c3921f4c866513411445149079be0a3
Cameron <github@strixdesigns.com>
2020-06-25 05:08:22 -0700

    update some tpa stuff nothing final

--- src/main/java/dev/excl/mc/fivecore/FiveCore.java
+++ src/main/java/dev/excl/mc/fivecore/FiveCore.java
@@ -6,10 +6,11 @@ import dev.excl.mc.fivecore.cmds.teleport.TpaCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FiveCore extends JavaPlugin {
-    private FiveCore Instance;
+    private static FiveCore Instance;
     Logger Logger = Bukkit.getLogger();
     ConsoleCommandSender clogger = this.getServer().getConsoleSender(); // must type .sendMessage(color + "Message") after it
     @Override
@@ -17,8 +18,20 @@ public final class FiveCore extends JavaPlugin {
         clogger.sendMessage(ChatColor.RED + "---------------------------------------");
         clogger.sendMessage(ChatColor.GREEN + "FiveCore Has Been Enabled");
         clogger.sendMessage(ChatColor.RED + "---------------------------------------");
-         Instance = this;
+
+        Instance = this;
+        /*
+        * Save Default Config File if it doesnt exist
+        * */
+        FileConfiguration config = this.getConfig();
+        this.saveDefaultConfig();
+        System.out.println(config.getString("test"));
+
+
         PaperCommandManager manager = new PaperCommandManager(Instance);
+        /*
+        * Command Registration
+        * */
         manager.registerCommand(new TpaCommand());
     }
 
@@ -28,7 +41,7 @@ public final class FiveCore extends JavaPlugin {
         clogger.sendMessage(ChatColor.RED + "FiveCore Has Been Disabled");
         clogger.sendMessage(ChatColor.GREEN + "---------------------------------------");
     }
-    public FiveCore getInstance() {
+    public static FiveCore getInstance() {
         return Instance;
     }
 }
--- src/main/java/dev/excl/mc/fivecore/cmds/teleport/TpaCommand.java
+++ src/main/java/dev/excl/mc/fivecore/cmds/teleport/TpaCommand.java
@@ -3,6 +3,7 @@ package dev.excl.mc.fivecore.cmds.teleport;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.*;
 import co.aikar.commands.bukkit.contexts.OnlinePlayer;
+import dev.excl.mc.fivecore.FiveCore;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -20,9 +21,18 @@ public class TpaCommand extends BaseCommand {
             player.sendMessage(ChatColor.RED + "You must teleport to an online player");
             return;
         }
+        // DELAYED TASK (only called once)
+
+        FiveCore.getInstance().getServer().getScheduler().scheduleSyncDelayedTask(FiveCore.getInstance(), new Runnable() {
+            public void run() {
+                System.out.println("Runnable Test");
+                // do other stuff...
+            }
+        }, 600L);
         Player player2 = target.getPlayer();
-        player.sendMessage(ChatColor.RED + player2.getDisplayName());
+        player.sendMessage(ChatColor.RED + player2.getDisplayName() + " test3");
     }
 
 }
+
 
